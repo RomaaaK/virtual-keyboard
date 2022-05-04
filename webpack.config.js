416f7817c257dd777e18593b81a5d9b16baf88af
RomaaaK <zimpsonzya@gmail.com>
2022-05-04 22:12:58 +0300

    feat: add normalize

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   entry: path.resolve(__dirname, './src/index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[hash].bundle.js',
+    filename: 'bundle.js',
     clean: true,
   },
   module: {
